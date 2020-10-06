2951c04e806c3afbe73d3017be00708a0193f1a9
watanabe nami <210392v@ugs.kochi-tech.ac.jp>
2020-10-06 14:49:52 +0900

    通知：音を録音にする以外はできた

--- VoiceReminder/DetailsViewController.swift
+++ VoiceReminder/DetailsViewController.swift
@@ -13,11 +13,12 @@ class DetailsViewController: UIViewController{
     @IBOutlet weak var timePicker: UIDatePicker! //時間選択
     @IBOutlet var memoTextField: UITextField! //タイトル入力
 
-    
     var saveData: UserDefaults = UserDefaults.standard
     var stringDatetime: String!
     var stringDatePicker: String!
     var stringTimePicker: String!
+    var request: UNNotificationRequest!
+    let calendar: Calendar = Calendar(identifier: .gregorian)
     
     //最初からあるメソッド
     override func viewDidLoad() {
@@ -37,10 +38,13 @@ class DetailsViewController: UIViewController{
     func changeStringDate(){
         let formatDate = DateFormatter()
         let formatTime = DateFormatter()
+        
         formatDate.dateFormat = "yyyy年MM年dd日"
         formatTime.dateFormat = "HH:mm"
+        
         stringDatePicker = formatDate.string(from: datePicker.date)
         stringTimePicker = formatTime.string(from: timePicker.date)
+
     }
     
     @IBAction func save(){
@@ -54,6 +58,36 @@ class DetailsViewController: UIViewController{
         saveData.set(stringDatePicker, forKey: "date")
         saveData.set(stringTimePicker, forKey: "time")
         saveData.set(memoTextField.text, forKey: "memo")
+        tsuchi()
+        let center = UNUserNotificationCenter.current()
+        center.add(request)
         self.performSegue(withIdentifier: "toView", sender: nil)
+    }
+    
+    func tsuchi(){
+        let dateComponents = calendar.dateComponents([.year, .month, .day], from: datePicker.date)
+        let timeComponents = calendar.dateComponents([.hour, .minute], from: timePicker.date)
+        
+        let year = dateComponents.year!
+        let month = dateComponents.month!
+        let day = dateComponents.day!
+        let hour = timeComponents.hour!
+        let minute = timeComponents.minute!
+        
+        //通知日時を直接指定
+        let triggerDate = DateComponents(year: year, month: month, day: day, hour: hour, minute: minute, second: 0)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+        
+        //通知コンテンツの作成
+        let content = UNMutableNotificationContent()
+        content.title = "VoiceReminder"
+        content.body = saveData.object(forKey: "memo") as! String
+        content.sound = UNNotificationSound.default
+        
+        //すぐに通知を表示
+        request = UNNotificationRequest(
+            identifier: "CalendarNotification",
+            content: content,
+            trigger: trigger)
     }
 }
--- VoiceReminder/ViewController.swift
+++ VoiceReminder/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController{
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var timeLabel: UILabel!
     @IBOutlet var memoLabel: UILabel!
-    @IBOutlet var tsutiButton: UIButton!
+   
     var saveData: UserDefaults = UserDefaults.standard
    
     override func viewDidLoad() {
@@ -26,17 +26,6 @@ class ViewController: UIViewController{
     @IBAction func addButton(){
         
     }
-    
-    @IBAction func localpush(_ sender: Any){
-        let content = UNMutableNotificationContent()
-        content.title = "test"
-        content.body = "testBody"
-        content.sound = UNNotificationSound.default
-        //すぐに通知を表示
-        let request = UNNotificationRequest(identifier: "immediately", content: content, trigger: nil)
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
-    }
-
 
 }
 
